12e11b41d877d35c2f31efdba12e2aaa35738ec1
Gonzalo Terzano <gonzaloterzano@gmail.com>
2019-05-06 20:58:53 -0300

    S1: Creating basic and dummy components to display a product

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,17 @@
+require('./Gallery.scss');
+
+import React from 'react';
+
+class Gallery extends React.Component {
+  render() {
+    return (
+      <section className="gallery">
+        Gallery
+      </section>
+    );
+  }
+}
+
+Gallery.defaultProps = {};
+
+export default Gallery;
