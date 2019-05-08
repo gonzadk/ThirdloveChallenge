e6150ed20be243c5d1e3bb3e2f5775b131635ba4
Gonza Terzano <gonzaloterzano@gmail.com>
2019-05-08 13:44:43 -0300

    S10: Updating dependencies to newest ones

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,9 +1,13 @@
-require('./ProductDetails.scss');
-
-import DOMPurify from 'dompurify';
 import React from 'react';
+import DOMPurify from 'dompurify';
 
-class ProductDetails extends React.Component {
+import './ProductDetails.scss';
+
+type MyProps = {
+  details: any
+};
+type MyState = {};
+class ProductDetails extends React.Component<MyProps, MyState> {
   render() {
     const { details } = this.props;
 
@@ -12,12 +16,10 @@ class ProductDetails extends React.Component {
         <h2>Details</h2>
 
         {/* Avoid this kind of insertions if it is project in production */}
-        <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(details)}}></div>
+        <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(details) }}/>
       </section>
     );
   }
 }
-
-ProductDetails.defaultProps = {};
 
 export default ProductDetails;
