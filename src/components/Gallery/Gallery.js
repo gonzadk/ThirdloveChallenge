0573b953e16d5854b8f0604389ce1567c277471a
Gonzalo Terzano <gonzaloterzano@gmail.com>
2019-05-08 10:47:03 -0300

    S9: Showing gallery with right images

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,12 +1,29 @@
 require('./Gallery.scss');
 
+import ImageGallery from 'react-image-gallery';
 import React from 'react';
+import * as _ from 'lodash';
+
+const HTTPS = 'https://';
 
 class Gallery extends React.Component {
   render() {
+    const isDesktop = false;
+    const images = _.map(this.props.images, image => ({
+      original: `${HTTPS}${image.src1000}`,
+      thumbnail: `${HTTPS}${image.src100}`,
+      bulletClass: 'bullet-class'
+    }));
+
     return (
       <section className="gallery">
-        Gallery
+        <ImageGallery items={images}
+                      showNav={false}
+                      showThumbnails={isDesktop}
+                      thumbnailPosition={'left'}
+                      showFullscreenButton={false}
+                      showPlayButton={false}
+                      showBullets={!isDesktop}/>
       </section>
     );
   }
