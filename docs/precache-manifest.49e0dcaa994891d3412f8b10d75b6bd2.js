22c66a350f5b33cafad73b56dddb377aac07f287
Gonzalo Terzano <gonzaloterzano@gmail.com>
2019-05-11 20:00:55 -0300

    S17: Updating build files with initial path

--- docs/precache-manifest.49e0dcaa994891d3412f8b10d75b6bd2.js
+++ docs/precache-manifest.49e0dcaa994891d3412f8b10d75b6bd2.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f77a4c55b23df7af1ae0528ae2af9bbd",
-    "url": "/index.html"
+    "url": "/ThirdloveChallenge/index.html"
   },
   {
     "revision": "adecf37f48016fa438f2",
-    "url": "/static/css/main.400aad98.chunk.css"
+    "url": "/ThirdloveChallenge/static/css/main.400aad98.chunk.css"
   },
   {
     "revision": "3024a390b8ee46478ce8",
-    "url": "/static/js/2.7cc78901.chunk.js"
+    "url": "/ThirdloveChallenge/static/js/2.7cc78901.chunk.js"
   },
   {
     "revision": "adecf37f48016fa438f2",
-    "url": "/static/js/main.10759760.chunk.js"
+    "url": "/ThirdloveChallenge/static/js/main.10759760.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/ThirdloveChallenge/static/js/runtime~main.a8a9905a.js"
   }
 ]);
