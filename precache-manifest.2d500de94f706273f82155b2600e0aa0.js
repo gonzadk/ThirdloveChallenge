3137a73fecf9b9f3b207b3ac90d2668ebec2c4df
Gonzalo Terzano <gonzaloterzano@gmail.com>
2019-05-11 22:23:21 -0300

    Updates

---
+++ precache-manifest.2d500de94f706273f82155b2600e0aa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40102e896874b7c90196a4d0757b028",
+    "url": "/ThirdloveChallenge/index.html"
+  },
+  {
+    "revision": "88cf919dfe11c91a692e",
+    "url": "/ThirdloveChallenge/static/css/main.b077ecdd.chunk.css"
+  },
+  {
+    "revision": "2940fb946e8861533374",
+    "url": "/ThirdloveChallenge/static/js/2.dd715ba9.chunk.js"
+  },
+  {
+    "revision": "88cf919dfe11c91a692e",
+    "url": "/ThirdloveChallenge/static/js/main.1448d83f.chunk.js"
+  },
+  {
+    "revision": "08389b42187a7b399784",
+    "url": "/ThirdloveChallenge/static/js/runtime~main.8f687873.js"
+  }
+]);
